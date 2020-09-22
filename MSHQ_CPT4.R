b846503a58034404462e72a20b67873ccb646e46
greglenane <gregory.lenane@mssm.edu>
2020-09-22 13:57:56 -0400

    Validation of Script

--- MSHQ_CPT4.R
+++ MSHQ_CPT4.R
@@ -5,45 +5,51 @@ library(readxl)
 library(lubridate)
 library(xlsx)
 
-#Bring in all sheets in charges file
-path <- file.choose()
-sheetnames <- excel_sheets(path)
-mylist <- lapply(excel_sheets(path), read_excel, path = path, col_names = F)
-names(mylist) <- sheetnames
-names(mylist)
-
 #Bring in Rev Dep to CC crosswalk
-rev_map <- read_excel("J:\\deans\\Presidents\\SixSigma\\MSHS Productivity\\Productivity\\Volume - Data\\MSH Data\\Charges\\MSHQ REV CROSSWALK.xlsx")
+rev_map <- read_excel("J:\\deans\\Presidents\\SixSigma\\MSHS Productivity\\Productivity\\Volume - Data\\MSH Data\\Charges\\MSHQ REV CROSSWALK.xlsx") %>%
+  select(c(1,5:8)) %>%
+  distinct()
 cpt_ref <- read_excel("J:\\deans\\Presidents\\SixSigma\\MSHS Productivity\\Productivity\\Volume - Data\\MSH Data\\Charges\\CPT Reference\\CPT_Ref.xlsx") %>%
   select(1,2,3,5,11,12)
 
-#Creates MSHQ Premier upload
+#Prepares file for master
 charges <- function(MSH,MSQ){
   chargenames <- c("SINAI.CODE","REV.DEP","DESCRIPTION","CPT","QTY","MONTH")
   colnames(MSH) <- chargenames
   colnames(MSQ) <- chargenames
   MSHQ <- rbind(MSH,MSQ)
   MSHQ <- filter(MSHQ,!is.na(CPT))
+  if(max(MSHQ$MONTH) < 4){
+    Q <<- 1
+  } else if(max(MSHQ$MONTH) < 7){
+    Q <<- 2
+  } else if(max(MSHQ$MONTH) < 10){
+    Q <<- 3
+  } else {
+    Q <<- 4
+  }
   MSHQ <- left_join(MSHQ,rev_map) %>% 
-    filter(!is.na(PREMIER)) %>%
+    filter(!is.na(ORACLE.CC)) %>%
     mutate(PARTNER = "729805",
            BUDGET = "0",
            HOSP = "NY0014",
-           START = as.Date(paste0(MONTH,"/1/",Year),format = "%m/%d/%Y"))
+           START = as.Date(paste0(MONTH,"/1/",Year),format = "%m/%d/%Y"),
+           QUARTER = Q)
   MSHQ <- mutate(MSHQ,
-                 END = paste0(MONTH,"/",days_in_month(MONTH),"/",Year),
-                 START = paste0(substr(START,6,7),"/",substr(START,9,10),"/",Year)) %>%
-    group_by(PARTNER,HOSP,ORACLE.CC,CPT,START,END,CPT,BUDGET,CPT.GROUP) %>%
+                 END = paste0(substr(START,6,7),"/",days_in_month(MONTH),"/",Year),
+                 START = paste0(substr(START,6,7),"/",substr(START,9,10),"/",Year),
+                 REP.DEFINITION = paste0(REP.ID," - ",REP.NAME)) %>%
+    group_by(PARTNER,HOSP,ORACLE.CC,CPT,START,END,CPT,BUDGET,CPT.GROUP,REP.DEFINITION,QUARTER) %>%
     summarise(QTY = sum(QTY, na.rm=T)) %>%
-    select(PARTNER,HOSP,ORACLE.CC,CPT,START,END,CPT,QTY,BUDGET,CPT.GROUP)
+    select(PARTNER,HOSP,ORACLE.CC,CPT,START,END,QTY,BUDGET,CPT.GROUP,REP.DEFINITION,QUARTER)
   MSHQ <<- MSHQ
 }
 #Creates master repository and master trend
-master <- function(Q){
+master <- function(){
   master <- readRDS("J:\\deans\\Presidents\\SixSigma\\MSHS Productivity\\Productivity\\Volume - Data\\MSH Data\\Charges\\Master\\master.RDS")
   master <- rbind(master,MSHQ)
   master_trend <- master %>%
-    mutate(`Concatenate for lookup` = paste0(Year,"Q",Q,CPT)) %>%
+    mutate(`Concatenate for lookup` = paste0(Year,"Q",QUARTER,CPT)) %>%
     left_join(.,cpt_ref) %>%
     mutate(LABOR = case_when(
       CPT.GROUP == "PROCEDURE" ~ QTY*`CPT Procedure Count`,
@@ -51,9 +57,9 @@ master <- function(Q){
       CPT.GROUP == "RVU" ~ QTY*`Facility Total RVU Factor`),
       LABOR = as.numeric(LABOR)) %>%
     filter(LABOR > 0) %>%
-    group_by(ORACLE.CC,END)%>%
-    summarise(LABOR = sum(LABOR,na.rm = T))%>%
-    pivot_wider(id_cols = ORACLE.CC,names_from = END,values_from = LABOR)
+    group_by(REP.DEFINITION,CPT.GROUP,END)%>%
+    summarise(LABOR = sum(LABOR,na.rm = T)) %>%
+    pivot_wider(id_cols = c(REP.DEFINITION,CPT.GROUP),names_from = END,values_from = LABOR)
   master <<- master
   master_trend <<- master_trend
 }
@@ -62,7 +68,7 @@ upload_master <- function(){
   saveRDS(master,"J:\\deans\\Presidents\\SixSigma\\MSHS Productivity\\Productivity\\Volume - Data\\MSH Data\\Charges\\Master\\master.rds")
   write.xlsx(as.data.frame(master_trend),"J:\\deans\\Presidents\\SixSigma\\MSHS Productivity\\Productivity\\Volume - Data\\MSH Data\\Charges\\Master\\master_trend.xlsx",
              row.names = F)
-  upload <- select(MSHQ,c(1:8))
+  upload <- MSHQ %>% ungroup() %>% select(c(1:8))
   start <- as.Date(min(MSHQ$START),format = "%m/%d/%Y")
   end <- as.Date(max(MSHQ$END),format = "%m/%d/%Y")
   smonth <- month(start)
@@ -77,12 +83,19 @@ upload_master <- function(){
   write.csv(upload,upload_path)
 }
 
-#Enter Current Year
+#Bring in all sheets in charges file
+path <- file.choose()
+sheetnames <- excel_sheets(path)
+mylist <- lapply(excel_sheets(path), read_excel, path = path, col_names = F)
+names(mylist) <- sheetnames
+names(mylist)
+
+#Enter Year of data
 Year <- "2020"
 #Execute functions
 charges(MSH = mylist[[1]],MSQ = mylist[[2]])
 #Create master and master trend
-master(Q = 3)
+master()
 #Review master trend
 
 #Create upload and save both master files and upload
